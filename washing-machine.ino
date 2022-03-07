93fc61244b568cfaa3241459a0502197b1ca27ce
TomRomeo <Tom.Romeo.Doil@gmail.com>
2022-03-08 00:10:28 +0100

    added wifi and discord webhook messages

--- washing-machine.ino
+++ washing-machine.ino
@@ -2,6 +2,8 @@
 #include <SPI.h>//https://www.arduino.cc/en/reference/SPI
 #include <MFRC522.h>//https://github.com/miguelbalboa/rfid
 #include <elapsedMillis.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
 //Constants
 #define SS_PIN 5
@@ -13,9 +15,15 @@ const int delayBetweenMeasure = 600000; // 10 minutes
 const int measureDuration = 60000; // 1 minute
 
 
-byte tomsTag[4] = {103, 97, 67, 25};
+byte tomsTag[4] = {115, 191, 129, 015};
 byte myTag2[4] = {103, 97, 67, 25};
 
+
+const char* ssid = "";
+const char* password = "";
+const char* webhookUrl = "";
+
+HTTPClient http;
 
 //Variables
 byte nuidPICC[4] = {0, 0, 0, 0};
@@ -36,13 +44,37 @@ void setup() {
 
   Serial.print(F("Reader :"));
   rfid.PCD_DumpVersionToSerial();
+
+  // connecting to wifi
+  WiFi.begin(ssid, password);
+ 
+  Serial.println("Establishing connection to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+  Serial.println("Connected to network with IP:");
+  Serial.println(WiFi.localIP());
+
+   sendDiscordMessage(
+     "{\
+       \"content\": null,\
+       \"embeds\": [\
+         {\
+           \"title\": \"Washing analyzer started up\",\
+           \"color\": 65280\
+         }\
+       ]\
+     }"
+   );
 }
 
 void loop() {
   readRFID();
 }
 
-void readRFID(void) { /* function readRFID */
+void readRFID() { /* function readRFID */
   
   // Scan for RFID card
   for (byte i = 0; i < 6; i++) {
@@ -86,6 +118,22 @@ void washingAnalysisLoop(){
   boolean isRunning = true;
   Serial.println("Washing loop startet");
 
+  char discordID[20];
+  tagToDiscordID(nuidPICC, discordID);
+  Serial.println(discordID);
+  
+  sendDiscordMessage(
+     "{\
+       \"content\": \"<@!" + String(discordID) + ">\", \
+       \"embeds\": [\
+         {\
+           \"title\": \"Washing monitoring started\",\
+           \"color\": 65280\
+         }\
+       ]\
+     }"
+   );
+
   while(isRunning == true){
     Serial.println("Waiting for " + String(delayBetweenMeasure) + " milliseconds");
     delay(delayBetweenMeasure);
@@ -124,5 +172,39 @@ void printDec(byte *buffer, byte bufferSize) {
 }
 
 void sendMessage(){
-  Serial.println("Washing maschine finished!");
+  Serial.println("Washing machine finished!");
+  char discordID[20];
+  tagToDiscordID(nuidPICC, discordID);
+  
+  sendDiscordMessage(
+     "{\
+       \"content\": \"<@!" + String(discordID) + ">\", \
+       \"embeds\": [\
+         {\
+           \"title\": \"Washing machine finished!\",\
+           \"color\": 65280\
+         }\
+       ]\
+     }"
+   );
+}
+
+void sendDiscordMessage(String content) {
+  http.begin(webhookUrl);
+  http.addHeader("Content-Type", "application/json");
+  http.POST(content);
+}
+
+void tagToDiscordID(byte tag[], char* outID) {
+    bool equals = true;
+    for (byte i = 0; i < 4; i++) {
+        if (tag[i] != tomsTag[i]){
+          equals = false;
+        } 
+    }
+    if (equals) {
+      memcpy(outID, "351460900370120707", sizeof("351460900370120707"));
+      return;
+    }
+    memcpy(outID, "000000000000000000", sizeof("000000000000000000"));
 }
